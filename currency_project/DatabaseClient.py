7329b4dafbbc8be0bf34583d1df82d71b0c8ff15
stanislaw_aniola <stanislaw.aniola.3mdeb@gmail.com>
2021-04-05 23:22:00 +0200

    currency_project/DatabaseClient.py: added table cration
    Signed-off-by: stanislaw_aniola <stanislaw.aniola.3mdeb@gmail.com>

--- currency_project/DatabaseClient.py
+++ currency_project/DatabaseClient.py
@@ -23,3 +23,35 @@ class DatabaseClient():
             print(e)
 
         return conn
+
+    def create_table(self, conn):
+        """
+        create currency_data and range_date tables
+        :param conn: Connection object
+        """
+        sql_create_currency_data_table = """ 
+                                         CREATE TABLE IF NOT EXISTS currency_data 
+                                         (
+                                             id integer PRIMARY KEY,
+                                             currency_name VARCHAR(255) NOT NULL,
+                                             date DATETIME NOT NULL,
+                                             price DOUBLE NOT NULL
+                                         ); 
+                                         """
+        sql_create_range_date_table = """ 
+                                 CREATE TABLE IF NOT EXISTS range_date 
+                                 (
+                                     currency_name VARCHAR(255) NOT NULL,
+                                     start_date DATETIME NOT NULL,
+                                     end_date DATETIME NOT NULL
+                                 ); 
+                                 """
+        cursor = conn.cursor()
+        try:
+            cursor.execute(sql_create_currency_data_table)
+            cursor.execute(sql_create_range_date_table)
+            print("Tables successfully created")
+        except Error as e:
+            print(e)
+        finally:
+            cursor.close()
